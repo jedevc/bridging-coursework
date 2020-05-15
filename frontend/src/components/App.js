af6ad351df8caf9387d5c06329b57c4ad272c3a7
Justin Chadwell <jedevc@gmail.com>
2020-05-15 17:02:42 +0100

    Add basic bulma stuff

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,14 +1,56 @@
 import React, { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0);
   return (
-    <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
-        Click me
-      </button>
-    </div>
+    <Hero title="Justin Chadwell" subtitle="Bridging Coursework" />
+  );
+}
+
+function Nav() {
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <div className="navbar-brand">
+          <span className="navbar-burger burger" data-target="navbarMenuHeroB">
+            <span></span>
+            <span></span>
+            <span></span>
+          </span>
+        </div>
+        <div className="navbar-menu">
+          <div className="navbar-end">
+            <a className="navbar-item is-active">Home</a>
+            <a className="navbar-item">Blog</a>
+            <a className="navbar-item">CV</a>
+            <a className="navbar-item">
+              <span className="button is-dark">
+                <span className="icon">
+                  <i className="fab fa-github"></i>
+                </span>
+                <span>GitHub</span>
+              </span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+function Hero(props) {
+  return (
+    <section className="hero is-primary is-medium">
+      <div className="hero-head">
+        <Nav />
+      </div>
+
+      <div className="hero-body">
+        <div className="container has-text-centered">
+          <h1 className="title">{props.title}</h1>
+          <h2 className="subtitle">{props.subtitle}</h2>
+        </div>
+      </div>
+    </section>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,5 +2,12 @@ import React from 'react';
 import { render } from "react-dom";
 import App from "./components/App";
 
+import 'bulma/css/bulma.css';
+
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
+
 const container = document.getElementById("app");
 render(<App />, container);
