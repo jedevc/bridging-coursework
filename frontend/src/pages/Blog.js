be269815d9510bb9f0bcfec10aa8ddf1afb98151
Justin Chadwell <jedevc@gmail.com>
2020-05-18 11:47:17 +0100

    Add date to post

--- frontend/src/pages/Blog.js
+++ frontend/src/pages/Blog.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
 
+import { formatDate } from "../utils/date";
+
 export default function Blog() {
   const [posts, setPosts] = useState([]);
 
@@ -33,23 +35,11 @@ export default function Blog() {
 function PostPreview(props) {
   return (
     <div className="box">
-      <div>
-        <h2 className="title" style={{marginBottom: "0.6rem"}}>{props.post.title}</h2>
-        <div className="has-text-grey is-size-7" style={{marginBottom: "1.2rem"}}>
-          <time dateTime={props.post.published_date}>{formatDate(props.post.published_date)}</time>
-        </div>
+      <h2 className="title" style={{marginBottom: "0.6rem"}}>{props.post.title}</h2>
+      <div className="has-text-grey is-size-7" style={{marginBottom: "1.2rem"}}>
+        <time dateTime={props.post.published_date}>{formatDate(props.post.published_date)}</time>
       </div>
       <p>{props.post.summary}</p>
     </div>
   )
-}
-
-function formatDate(dateString) {
-  const date = new Date(dateString);
-  const options = {
-    day: "numeric",
-    month: "long",
-    year: "numeric"
-  }
-  return date.toLocaleDateString('en-GB', options);
 }
--- frontend/src/pages/Post.js
+++ frontend/src/pages/Post.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { Link, useParams } from "react-router-dom";
 import ReactMarkdown from "react-markdown";
 
+import { formatDate } from "../utils/date";
+
 export default function Post() {
   const [post, setPost] = useState({});
   let { id } = useParams();
@@ -22,7 +24,10 @@ export default function Post() {
       <div className="container">
         <div className="columns">
           <div className="column is-three-quarters-desktop is-three-quarters-tablet is-full-mobile">
-            <h1 className="title">{post.title}</h1>
+            <h1 className="title" style={{marginBottom: "0.6rem"}}>{post.title}</h1>
+            <div className="has-text-grey is-size-7" style={{marginBottom: "1.2rem"}}>
+              <time dateTime={post.published_date}>{formatDate(post.published_date)}</time>
+            </div>
 
             <div className="content">
               <ReactMarkdown source={post.content} />
