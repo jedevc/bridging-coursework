9f5dfb5e02ddd8bf1f0b42530107bbe1591dd78e
Justin Chadwell <jedevc@gmail.com>
2020-05-27 10:38:00 +0100

    Add font awesome from CDN

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,11 +4,6 @@ import App from "./App";
 
 import "regenerator-runtime/runtime";
 
-import '@fortawesome/fontawesome-free/js/fontawesome'
-import '@fortawesome/fontawesome-free/js/solid'
-import '@fortawesome/fontawesome-free/js/regular'
-import '@fortawesome/fontawesome-free/js/brands'
-
 import "../styles/main.scss";
 
 const container = document.getElementById("app");
